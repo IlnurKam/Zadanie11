9ceb06c9898582a4845e5588f643ae808459f6de
IlnurKam <kamal673@yandex.ru>
2022-01-26 20:57:30 +0300

    Добавьте файлы проекта.

---
+++ Zadanie11/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Equation lineEquation = new Equation();
+            try
+            {
+                Console.WriteLine("Решение линейного уравнения 0=kx+b ");
+
+                Console.Write("Введите коэффициент k: ");
+                lineEquation.CoefK = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите коэффициент b: ");
+                lineEquation.CoefB = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода!");
+                return;
+            }
+            lineEquation.Solution();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11
+{
+    
+    struct Equation
+    {
+        private double _coefK;
+
+        public double CoefK
+        {
+            get
+            {
+                return _coefK;
+            }
+            set
+            {
+                if (value != 0)
+                {
+                    _coefK = value;
+                }
+                else
+                {
+                    Console.WriteLine("k не может быть равен 0");
+                }
+            }
+        }
+        public double CoefB { get; set; }
+        public Equation(double CoefK, double CoefB)
+        {
+            this._coefK = CoefK;
+            this.CoefB = CoefB;
+        }
+        public void Solution()
+        {
+            Console.WriteLine("Х={0:f2}", (-CoefB) / CoefK);
+        }
+    }
+}
